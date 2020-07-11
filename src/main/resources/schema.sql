02f6a2019a6867ec234f745e5b7f6add2c53b3a9
Pavel Vorobev <p.vorobyev@inbox.ru>
2020-07-11 17:41:37 +0300

    переход на postgresql, схема инициализации, jdbc-core

---
+++ src/main/resources/schema.sql
@@ -0,0 +1,174 @@
+drop table if exists tasks cascade;
+drop table if exists stories cascade;
+drop table if exists epics cascade;
+drop table if exists bugs cascade;
+drop table if exists project_users;
+drop table if exists projects;
+drop table if exists sprint;
+drop table if exists backlog;
+drop table if exists user_roles;
+drop table if exists users;
+
+create table users
+(
+    id       serial not null,
+    email    varchar(255) not null,
+    name     varchar(255) not null,
+    password varchar(255) not null,
+    constraint users_pkey primary key (id),
+    constraint uk_email unique (email)
+);
+
+create table user_roles
+(
+    user_id integer not null,
+    roles   varchar(255),
+    constraint fk_user_roles_user_id_users foreign key (user_id) references users (id)
+        on delete cascade
+);
+
+create table backlog
+(
+    id serial not null,
+    constraint backlog_pkey primary key (id)
+);
+
+create table sprint
+(
+    id serial not null,
+    constraint sprint_pkey primary key (id)
+);
+
+create table projects
+(
+    id            serial not null,
+    administrator varchar(255) not null,
+    department    varchar(255) not null,
+    description   varchar(255) not null,
+    manager       varchar(255) not null,
+    name          varchar(255) not null,
+    backlog_id    integer,
+    sprint_id     integer,
+    constraint projects_pkey primary key (id),
+    constraint fk_projects_backlog_id_backlog foreign key (backlog_id) references backlog (id),
+    constraint fk_projects_sprint_id_sprint foreign key (sprint_id) references sprint (id)
+);
+
+create table project_users
+(
+    project_id integer not null,
+    user_id    integer not null,
+    constraint project_users_pkey primary key (project_id, user_id),
+    constraint fk_project_users_project_id_project foreign key (user_id) references users (id)
+        on delete cascade,
+    constraint fk_project_users_user_id_users foreign key (project_id) references projects (id)
+        on delete cascade
+);
+
+create table bugs
+(
+    id            serial not null,
+    creation_date timestamp not null,
+    name          varchar(255) not null,
+    priority      integer not null,
+    status        varchar(255) not null,
+    executor_id   integer,
+    reporter_id   integer,
+    backlog_id    integer,
+    root_epic_id  integer,
+    root_story_id integer,
+    root_task_id  integer,
+    sprint_id     integer,
+    constraint bugs_pkey primary key (id),
+    constraint fk_bugs_executor_id_users foreign key (executor_id) references users (id),
+    constraint fk_bugs_reporter_id_users foreign key (reporter_id) references users (id),
+    constraint fk_bugs_backlog_id_backlog foreign key (backlog_id) references backlog (id)
+        on delete cascade,
+    constraint fk_bugs_sprint_id_sprint foreign key (sprint_id) references sprint (id)
+        on delete cascade
+);
+
+create table epics
+(
+    id            serial not null,
+    creation_date timestamp not null,
+    name          varchar(255) not null,
+    priority      integer not null,
+    status        varchar(255) not null,
+    executor_id   integer,
+    reporter_id   integer,
+    backlog_id    integer,
+    root_bug_id   integer,
+    root_story_id integer,
+    root_task_id  integer,
+    sprint_id     integer,
+    constraint epics_pkey primary key (id),
+    constraint fk_epics_executor_id_users foreign key (executor_id) references users (id),
+    constraint fk_epics_reporter_id_users foreign key (reporter_id) references users (id),
+    constraint fk_epics_backlog_id_backlog foreign key (backlog_id) references backlog (id)
+        on delete cascade,
+    constraint fk_epics_sprint_id_sprint foreign key (sprint_id) references sprint (id)
+        on delete cascade
+);
+
+create table stories
+(
+    id            serial not null,
+    creation_date timestamp not null,
+    name          varchar(255) not null,
+    priority      integer not null,
+    status        varchar(255) not null,
+    executor_id   integer,
+    reporter_id   integer,
+    backlog_id    integer,
+    root_bug_id   integer,
+    root_epic_id  integer,
+    root_task_id  integer,
+    sprint_id     integer,
+    constraint stories_pkey primary key (id),
+    constraint fk_stories_executor_id_users foreign key (executor_id) references users (id),
+    constraint fk_stories_reporter_id_users foreign key (reporter_id) references users (id),
+    constraint fk_stories_backlog_id_backlog foreign key (backlog_id) references backlog (id)
+        on delete cascade,
+    constraint fk_stories_sprint_id_sprint foreign key (sprint_id) references sprint (id)
+        on delete cascade
+);
+
+create table tasks
+(
+    id            serial not null,
+    creation_date timestamp not null,
+    name          varchar(255) not null,
+    priority      integer not null,
+    status        varchar(255) not null,
+    executor_id   integer,
+    reporter_id   integer,
+    backlog_id    integer,
+    root_bug_id   integer,
+    root_epic_id  integer,
+    root_story_id integer,
+    sprint_id     integer,
+    constraint tasks_pkey primary key (id),
+    constraint fk_tasks_executor_id_users foreign key (executor_id) references users (id),
+    constraint fk_tasks_reporter_id_users foreign key (reporter_id) references users (id),
+    constraint fk_tasks_backlog_id_backlog foreign key (backlog_id) references backlog (id)
+        on delete cascade,
+    constraint fk_tasks foreign key (sprint_id) references sprint (id)
+        on delete cascade
+);
+
+alter table bugs add constraint fk_bugs_root_epic_id_epics foreign key (root_epic_id) references epics (id);
+alter table bugs add constraint fk_bugs_root_story_id_stories foreign key (root_story_id) references stories (id);
+alter table bugs add constraint fk_bugs_root_task_id_tasks foreign key (root_task_id) references tasks (id);
+
+alter table epics add constraint fk_epics_root_bug_id_bugs foreign key (root_bug_id) references bugs (id);
+alter table epics add constraint fk_epics_root_story_id_stories foreign key (root_story_id) references stories (id);
+alter table epics add constraint fk_epics_task_id_tasks foreign key (root_task_id) references tasks (id);
+
+alter table stories add constraint fk_stories_root_bug_id_bugs foreign key (root_bug_id) references bugs (id);
+alter table stories add constraint fk_stories_root_epic_id_epics foreign key (root_epic_id) references epics (id);
+alter table stories add constraint fk_stories_task_id_tasks foreign key (root_task_id) references tasks (id);
+
+alter table tasks add constraint fk_tasks_root_bug_id_bugs foreign key (root_bug_id) references bugs (id);
+alter table tasks add constraint fk_tasks_root_epic_id_epics foreign key (root_epic_id) references epics (id);
+alter table tasks add constraint fk_tasks_root_story_id_stories foreign key (root_story_id) references stories (id);
