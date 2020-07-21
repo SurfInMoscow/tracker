4fa4ed12476e7a846e3d8068702c4171439a812f
Pavel Vorobev <p.vorobyev@inbox.ru>
2020-07-21 12:48:25 +0300

    рефакторинг реализации inmemory для backlog

--- src/main/java/ru/vorobyev/tracker/repository/inmemory/project/BacklogRepositoryImpl.java
+++ src/main/java/ru/vorobyev/tracker/repository/inmemory/project/BacklogRepositoryImpl.java
@@ -55,26 +55,51 @@ public class BacklogRepositoryImpl implements BacklogRepository {
         backlog.setEpics(backlog.getEpics().stream().filter(e -> e.getPriority().name().equals(priority)).collect(Collectors.toSet()));
         backlog.setStories(backlog.getStories().stream().filter(s -> s.getPriority().name().equals(priority)).collect(Collectors.toSet()));
         backlog.setTasks(backlog.getTasks().stream().filter(t -> t.getPriority().name().equals(priority)).collect(Collectors.toSet()));
+
         return backlog;
     }
 
     @Override
     public Backlog getWithIssuesBetweenDates(int id, LocalDateTime startDate, LocalDateTime endDate) {
-        return null;
+        Backlog backlog = backlogRepo.get(id);
+        backlog.setBugs(backlog.getBugs().stream().filter(b -> b.getCreationDate().isAfter(startDate) && b.getCreationDate().isBefore(endDate)).collect(Collectors.toSet()));
+        backlog.setEpics(backlog.getEpics().stream().filter(e -> e.getCreationDate().isAfter(startDate) && e.getCreationDate().isBefore(endDate)).collect(Collectors.toSet()));
+        backlog.setStories(backlog.getStories().stream().filter(s -> s.getCreationDate().isAfter(startDate) && s.getCreationDate().isBefore(endDate)).collect(Collectors.toSet()));
+        backlog.setTasks(backlog.getTasks().stream().filter(t -> t.getCreationDate().isAfter(startDate) && t.getCreationDate().isBefore(endDate)).collect(Collectors.toSet()));
+
+        return backlog;
     }
 
     @Override
     public Backlog getWithIssuesByName(int id, String name) {
-        return null;
+        Backlog backlog = backlogRepo.get(id);
+        backlog.setBugs(backlog.getBugs().stream().filter(b -> b.getName().equals(name)).collect(Collectors.toSet()));
+        backlog.setEpics(backlog.getEpics().stream().filter(e -> e.getName().equals(name)).collect(Collectors.toSet()));
+        backlog.setStories(backlog.getStories().stream().filter(s -> s.getName().equals(name)).collect(Collectors.toSet()));
+        backlog.setTasks(backlog.getTasks().stream().filter(t -> t.getName().equals(name)).collect(Collectors.toSet()));
+
+        return backlog;
     }
 
     @Override
     public Backlog getWithIssuesByExecutor(int id, int executor_id) {
-        return null;
+        Backlog backlog = backlogRepo.get(id);
+        backlog.setBugs(backlog.getBugs().stream().filter(b -> b.getExecutor().getId() == executor_id).collect(Collectors.toSet()));
+        backlog.setEpics(backlog.getEpics().stream().filter(e -> e.getExecutor().getId() == executor_id).collect(Collectors.toSet()));
+        backlog.setStories(backlog.getStories().stream().filter(s -> s.getExecutor().getId() == executor_id).collect(Collectors.toSet()));
+        backlog.setTasks(backlog.getTasks().stream().filter(t -> t.getExecutor().getId() == executor_id).collect(Collectors.toSet()));
+
+        return backlog;
     }
 
     @Override
     public Backlog getWithIssuesByReporter(int id, int reporter_id) {
-        return null;
+        Backlog backlog = backlogRepo.get(id);
+        backlog.setBugs(backlog.getBugs().stream().filter(b -> b.getReporter().getId() == reporter_id).collect(Collectors.toSet()));
+        backlog.setEpics(backlog.getEpics().stream().filter(e -> e.getReporter().getId() == reporter_id).collect(Collectors.toSet()));
+        backlog.setStories(backlog.getStories().stream().filter(s -> s.getReporter().getId() == reporter_id).collect(Collectors.toSet()));
+        backlog.setTasks(backlog.getTasks().stream().filter(t -> t.getReporter().getId() == reporter_id).collect(Collectors.toSet()));
+
+        return backlog;
     }
 }
