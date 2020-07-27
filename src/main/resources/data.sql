0b36522b21e6e48679dc114618b8082d89fe45c6
Pavel Vorobev <p.vorobyev@inbox.ru>
2020-07-27 11:59:09 +0300

    добавлен скрипт наполнения БД тестовыми данными

---
+++ src/main/resources/data.sql
@@ -0,0 +1,46 @@
+DELETE FROM projects;
+DELETE FROM backlog;
+DELETE FROM sprint;
+DELETE FROM users;
+
+ALTER SEQUENCE global_seq RESTART WITH 100000;
+
+INSERT INTO users(email, name, password) VALUES
+('user@ya.ru', 'user', 'password'),
+('test@inbox.ru', 'tester', 'testpass'),
+('hello@gmail.ru', 'worker', 'workerpass');
+
+INSERT INTO user_roles(user_id, roles) VALUES
+(100000, 'ROLE_USER'),
+(100001, 'ROLE_USER'),
+(100002, 'ROLE_USER');
+
+INSERT INTO backlog DEFAULT VALUES;
+INSERT INTO backlog DEFAULT VALUES;
+
+INSERT INTO sprint DEFAULT VALUES;
+INSERT INTO sprint DEFAULT VALUES;
+
+INSERT INTO projects(administrator, department, description, manager, name, backlog_id, sprint_id) VALUES
+('user@ya.ru', 'google ml', 'machine learning', 'hello@gmail.ru', 'Google ML', 100003, 100005),
+('test@inbox.ru', 'Apple Inc', 'Apple VR', 'test@inbox.ru', 'Apple VR', 100004, 100006);
+
+INSERT INTO project_users(project_id, user_id) VALUES
+(100007, 100000),
+(100007, 100002),
+(100008, 100000),
+(100008, 100001);
+
+INSERT INTO bugs(creation_date, name, priority, status, executor_id, reporter_id, backlog_id,
+                 root_epic_id, root_story_id, root_task_id, sprint_id) VALUES
+('2020-05-20 10:00:00', 'bug1', 'LOW', 'OPEN_ISSUE', 100001, 100000, 100003, NULL, NULL, NULL, NULL),
+('2020-05-23 15:00:00', 'bug2', 'MEDIUM', 'REVIEW_ISSUE', 100000, 100001, 100003, NULL, NULL, NULL, NULL),
+('2020-05-23 15:00:00', 'bugSprint', 'MEDIUM', 'RESOLVED_ISSUE', 100000, 100001, NULL, NULL, NULL, NULL, 100005),
+('2020-05-23 15:00:00', 'bug3', 'MEDIUM', 'TEST_ISSUE', 100002, 100000, 100004, NULL, NULL, NULL, NULL);
+
+INSERT INTO epics(creation_date, name, priority, status, executor_id, reporter_id, backlog_id,
+                  root_bug_id, root_story_id, root_task_id, sprint_id) VALUES
+('2020-04-20 10:00:00', 'epic1', 'LOW', 'OPEN_ISSUE', 100001, 100000, 100003, NULL, NULL, NULL, NULL),
+('2020-06-23 15:00:00', 'epic2', 'MEDIUM', 'REVIEW_ISSUE', 100000, 100001, 100003, NULL, NULL, NULL, NULL),
+('2020-05-23 15:00:00', 'epicSprint', 'MEDIUM', 'RESOLVED_ISSUE', 100000, 100001, NULL, NULL, NULL, NULL, 100005),
+('2020-05-25 15:00:00', 'epic3', 'MEDIUM', 'TEST_ISSUE', 100002, 100000, 100004, NULL, NULL, NULL, NULL);
