1b994c5ef68a6dbf36cf53ad093ec43c513699eb
Pavel Vorobev <p.vorobyev@inbox.ru>
2020-08-02 23:57:28 +0300

    рефакторинг и разделение на профили dev, prod(в данном профиле выключается отображение sql, отменяется пресоздание БД и активируется ssl)

--- src/main/java/ru/vorobyev/tracker/web/RegistrationController.java
+++ src/main/java/ru/vorobyev/tracker/web/RegistrationController.java
@@ -1,25 +1,12 @@
 package ru.vorobyev.tracker.web;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import ru.vorobyev.tracker.service.UserService;
 
 @Controller
 @RequestMapping("/tracker")
 public class RegistrationController {
-
-    private final UserService userService;
-    private final AuthenticationManager authenticationManagerBean;
-
-    @Autowired
-    public RegistrationController(UserService userService, AuthenticationManager authenticationManagerBean) {
-        this.userService = userService;
-        this.authenticationManagerBean = authenticationManagerBean;
-    }
-
     @GetMapping
     protected String login() {
         return "login";
